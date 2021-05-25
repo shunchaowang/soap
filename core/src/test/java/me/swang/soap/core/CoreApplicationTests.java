124a58fc8131766e634f6fce789ae48897909fa4
Shunchao Wang <shunchao.wang@gmail.com>
2021-05-24 22:35:47 -0400

    initial import

---
+++ core/src/test/java/me/swang/soap/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package me.swang.soap.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service/src/test/java/me/swang/soap/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.swang.soap.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
